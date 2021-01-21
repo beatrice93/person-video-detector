78dfb2dfe66c7262c2e658965b39baec23da4e39
beatrice93 <beatrice@chetard.fr>
2021-01-21 21:26:47 +0100

    fix more typos

--- detect_humans.py
+++ detect_humans.py
@@ -55,15 +55,15 @@ def check_config(args):
         print("Downloading weights...")
         wget.download("https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights",
                       out=yolo_folder + "/" + "yolov4.weights")
-    if not os.path.isfile(yolo_folder + "coco.names"):
+    if not os.path.isfile(yolo_folder + "/" +"coco.names"):
         print("\nDownloading class names...")
         wget.download("https://github.com/pjreddie/darknet/blob/master/data/coco.names",
                      out=yolo_folder + "/" + "coco.names" )
-    if not os.path.isfile(yolo_folder + "yolov4.cfg"):
+    if not os.path.isfile(yolo_folder + "/" + "yolov4.cfg"):
         print("\nDownloading config file...")
         wget.download("https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4.cfg",
                      out=yolo_folder + "/" + "yolov4.cfg")
-    print("Done.\n")
+    print("\nDone.\n")
 
     
 def detect(frame, net, output_layers):
--- readme.md
+++ readme.md
@@ -9,10 +9,20 @@ Requires openCV version 3 or higher.
 
 
 
+To run the script:
+
+- download the `detect_humans.py` file
+
+- in a terminal, run 
+
+  ```python detect_humans.py -i "path/to/video" -o "path/to/output" -c "path/to/config/folder" -d```
+
+- sit back and watch :)
+
 The script takes the following arguments:
  - `-i` path to input file (required)
  - `-o` path to output file (optional)
- - `-c` path to YOLOv4 configuration folder (optional, defaults to `yolov4_cfg/`)
+ - `-c` path to YOLOv4 configuration folder (optional, defaults to `yolov4_cfg`)
  - `-d` option to display the video as it is parsed
 
   
